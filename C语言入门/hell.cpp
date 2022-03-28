52eed514ed72dcdfb1f2767821865a45b776600d
Soeng.Peng <60353745+Guyue-Peng@users.noreply.github.com>
2022-03-27 21:08:30 -0700

    Add files via upload

---
+++ C语言入门/hell.cpp
@@ -0,0 +1,13 @@
+#include   <stdio.h>
+
+/* 
+ * comment1
+ * main: generate some simple output
+ */
+
+int main(void)
+{
+printf("character: %c\ninteger: %d\nfloating point: %f\n", '}', 34, 3.14);
+    
+return 0;
+}
